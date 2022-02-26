cfbbdc3b6bfb9f2ba1593d62dee1d66c8eaab2fb
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-02-26 14:13:40 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnviroment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+
+}
