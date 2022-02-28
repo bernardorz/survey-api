613e4266593ae513d8e8813fe16f3bb54942d90d
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-02-28 15:09:34 -0300

    test: ensure EmailValidatorAdapter calls correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -22,4 +22,11 @@ describe('EmailValidator Adapter', () => {
         const isValid = sut.isValid('valid_email@email.com')
         expect(isValid).toBe(true)
     })
+
+    test("Should call call validor with correct email", () => {
+        const sut = new EmailValidatorAdapter()
+        const isEmail = jest.spyOn(validator, 'isEmail')
+        sut.isValid('any_email@email.com')
+        expect(isEmail).toHaveBeenCalledWith('any_email@email.com')
+    })
 })
