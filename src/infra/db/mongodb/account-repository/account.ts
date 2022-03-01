7e8280fde43a05d6d6dbe871cf36f87f76856af2
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-01 14:08:00 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from 'domain/models/account'
+import { AddAcountModel } from 'domain/usecases/add-account'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+
+export class AccountMongoRepository implements AddAccountRepository{
+    public async add(accountData: AddAcountModel): Promise<AccountModel> {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        const result = await accountCollection.insertOne(accountData)
+        const account = MongoHelper.map(result.ops[0])
+        return account
+    }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -12,5 +12,18 @@ export const MongoHelper = {
   async disconnect (): Promise<void> {
     await this.client.close()
     this.client = null
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map: (data: any): any => {
+    const { _id, ...rest } = data
+    return { ...rest, id: _id.toHexString() }
+  },
+  
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map(c => MongoHelper.map(c))
   }
 }
