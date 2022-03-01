9f6f6170a3019bb143fdf5926a656819c0334c3e
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-01 14:08:22 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/accout.spec.ts
@@ -0,0 +1,32 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account mongo repository', () => {
+
+
+
+    beforeAll( async () => {
+        await MongoHelper.connect(global.__MONGO_URI__)
+    })
+
+    afterAll( async () => {
+        await MongoHelper.disconnect()
+    })
+
+    test('Should return an account on success', async () => {
+        const sut = new AccountMongoRepository()
+
+        const account = await sut.add({
+            name: 'any_name',
+            email: 'any_email@email.com',
+            password: 'any_password'
+        })
+
+        expect(account).toBeTruthy()
+        expect(account).toHaveProperty('id')
+        expect(account.name).toBe('any_name')
+        expect(account.email).toBe('any_email@email.com')
+        expect(account).toHaveProperty('id')
+    })
+
+})
