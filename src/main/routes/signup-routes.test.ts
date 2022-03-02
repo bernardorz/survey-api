11e2286921ee17b273c575f8864f75a4c686eeb1
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-01 22:41:00 -0300

    test: signUpRoutes returns 200 on sucess

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 
 import app from '../config/app'
-import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 
 describe('SignUp Routes', () => {
