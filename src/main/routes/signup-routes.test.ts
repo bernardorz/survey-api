9aacd40999978e64f130d6b48a743c9398d0cb5c
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-01 22:24:29 -0300

    test: ensure SignUp route retuns as account on sucess

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+
+describe('SignUp Routes', () => {
+    test('Should return an account on sucess', async () => {
+        await request(app)
+            .post('/api/signup')
+            .send({
+                name: "Bernardo",
+                email: "bernardorizzatti01@gmail.com",
+                password: "123",
+                passwordConfirmation: "123"
+            })
+            .expect(200)
+    })
+})
