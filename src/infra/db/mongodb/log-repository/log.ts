8f21f3f2e661bc6d958acb66ae55ded9d6b8d258
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-05 12:54:35 -0300

    feat: ensuere LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,12 @@
+import { LogErrorRepository} from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LoggerMongoRepository implements LogErrorRepository{
+    async logError(stackError: string): Promise<void> {
+       const errorCollection = await MongoHelper.getCollection('errors')
+       await errorCollection.insertOne({
+           stack: stackError,
+           date: new Date()
+       })
+    }
+}
