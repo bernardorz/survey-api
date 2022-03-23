c7ffcfa36826b279216430d04ad0e307d3578c19
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-23 08:16:03 -0300

    feat: ensure DbAuthentication call LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,13 @@
+import { LoadAccountByEmailRepository } from '@/data/protocols/load-account-by-email-repository';
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+
+
+export class DbAuthentication implements Authentication{
+    constructor(
+        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    ){}
+    async auth(authentication: AuthenticationModel): Promise<string> {
+        await this.loadAccountByEmailRepository.load(authentication.email)
+        return null
+    }
+}
