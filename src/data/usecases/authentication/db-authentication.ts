eab36c67fe0f46edcf548882cc588ff5b4b9f55e
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-23 10:42:43 -0300

    feat: ensure DbAuthentication calls HashCompar with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,13 +1,18 @@
-import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository';
+import { HashCompare } from '../../../data/protocols/cryptography/hash-compare-';
+import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository';
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 
 
 export class DbAuthentication implements Authentication{
     constructor(
-        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hashCompare: HashCompare
     ){}
     async auth(authentication: AuthenticationModel): Promise<string> {
-        await this.loadAccountByEmailRepository.load(authentication.email)
+        const account = await this.loadAccountByEmailRepository.load(authentication.email)
+        if(account){
+            await this.hashCompare.compare(authentication.password, account.password)
+        }
         return null
     }
 }
