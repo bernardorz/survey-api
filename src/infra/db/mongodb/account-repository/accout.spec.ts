6c5428d51d47fe6008070d087e22682d2cb6c6d0
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-24 21:34:27 -0300

    style: change eslintrc.json

--- src/infra/db/mongodb/account-repository/accout.spec.ts
+++ src/infra/db/mongodb/account-repository/accout.spec.ts
@@ -2,40 +2,36 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
 describe('Account mongo repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+  })
 
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
 
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
-    beforeAll( async () => {
-        await MongoHelper.connect(global.__MONGO_URI__)
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
     })
 
-    afterAll( async () => {
-        await MongoHelper.disconnect()
-    })
-
-    beforeEach(async () => {
-        const accountCollection = await MongoHelper.getCollection('accounts')
-        await accountCollection.deleteMany({})
-    })
-
-    const makeSut = () : AccountMongoRepository => {
-        return new AccountMongoRepository()
-    }
-
-    test('Should return an account on success', async () => {
-        const sut = makeSut()
-
-        const account = await sut.add({
-            name: 'any_name',
-            email: 'any_email@email.com',
-            password: 'any_password'
-        })
-
-        expect(account).toBeTruthy()
-        expect(account).toHaveProperty('id')
-        expect(account.name).toBe('any_name')
-        expect(account.email).toBe('any_email@email.com')
-        expect(account).toHaveProperty('id')
-    })
-
+    expect(account).toBeTruthy()
+    expect(account).toHaveProperty('id')
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account).toHaveProperty('id')
+  })
 })
