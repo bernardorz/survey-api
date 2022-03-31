cdab74d7040a08f1e1234d60e5f681f935e490d8
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-03-31 18:33:57 -0300

    chore: using envirroment to connect mongob

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: 'mongodb://root:root@localhost:27017/typeorm_db',
+    mongoUrl: process.env.MONGO_URL,
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'ASIDBPASIUDGA8PG287GA87T&7SADY7ASD7A6D7A6SD7A3&¨76&%$%&8ASDA8SDASUDGASYDGAWYDW'
 }
