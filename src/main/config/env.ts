ebc15ec7e2bf740a4c55e5c98b9ea3e492ac09ce
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-04-03 13:08:03 -0300

    chore: add docker-compose and debbug using Docker Attach

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL,
+    mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'ASIDBPASIUDGA8PG287GA87T&7SADY7ASD7A6D7A6SD7A3&¨76&%$%&8ASDA8SDASUDGASYDGAWYDW'
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,8 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
+console.log(env.mongoUrl)
+
 MongoHelper.connect(env.mongoUrl).then( async () => {
     const app = (await import('./config/app')).default
     app.listen(env.port, () => {
