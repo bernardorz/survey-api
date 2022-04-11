26ddd93594f3440061acddc5d11b973a3ed28cd9
bernardorz <bernardo.rizzatti@linkapi.com.br>
2022-04-11 17:03:05 -0300

    chore: add dbAuthentication use case in signup factory

--- src/main/factories/controllers/signup/signup-factory.ts
+++ src/main/factories/controllers/signup/signup-factory.ts
@@ -1,17 +1,27 @@
+import config from '../../../config/env'
+
+
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
 import { DbAddAcount } from '../../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../../infra/cryptography/bcrypter-adapter/bcrypt-adapter'
 import {  AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
 import { Controller  } from '../../../../presentation/protocols'
 import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
-import { LoggerMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { JwtAdapter } from '../../../../infra/cryptography/jwt-adapter/jwt-adapter'
+import {LoggerMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
+
+
 
 export const makeSignUpController = () : Controller => {
+    const accountDbRepository  = new AccountMongoRepository()
     const bcryptAdapter = new BcryptAdapter(12)
-    const addAcountRepository = new AccountMongoRepository() 
-    const dbAddAcount = new DbAddAcount(bcryptAdapter, addAcountRepository) 
-    const signUpController = new SignUpController(dbAddAcount, makeSignUpValidation())
+    const jwtAdapter = new JwtAdapter(config.jwtSecret)
+    const dbAuthentication = new DbAuthentication(accountDbRepository, bcryptAdapter, jwtAdapter, accountDbRepository)
+    const dbAddAcount = new DbAddAcount(bcryptAdapter, accountDbRepository) 
+    const signUpController = new SignUpController(dbAddAcount, makeSignUpValidation(), dbAuthentication)
     const loggerMongoRepository = new LoggerMongoRepository() 
+    
     return new LogControllerDecorator(signUpController, loggerMongoRepository)
 }
