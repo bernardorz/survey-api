a5ee8ea4f36b30f2c7f54e7bcbfeaf2b0f64fee9
bernardorz <bernardorizzatti01@gmail.com>
2022-09-13 17:44:15 -0300

    feat: return 400 if validation return true

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from "../../../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse, Validation } from "./add-survey-controller-protocols";
 
 export class AddSurveyController implements Controller{
@@ -9,7 +10,11 @@ export class AddSurveyController implements Controller{
     }
     public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 
-        this.validation.validate(httpRequest.body)
+       const error =  this.validation.validate(httpRequest.body)
+
+       if(error){
+           return badRequest(error)
+       }
 
         return new Promise(resolve => resolve(null))
         
